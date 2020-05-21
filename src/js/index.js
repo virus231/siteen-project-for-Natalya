98145d2968b95179a2f078e205d9d129855aa81b
Vlad <vladprodan6@gmail.com>
2020-05-22 01:43:04 +0300

    carousel for Sanya

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,11 @@ $("body").on("mouseenter", "[data-house]", function () {
     $(".house").removeClass("active");
     $("#"+$(this).data("house")+"").addClass("active");
 });
+
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+	
+  });
