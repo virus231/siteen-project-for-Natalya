883139c613eed808aa364cea1e6a76c3f52c12ef
Vlad <vladprodan6@gmail.com>
2020-05-22 02:01:23 +0300

    carousel!!!

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,16 @@ $("body").on("mouseenter", "[data-house]", function () {
 });
 
 var swiper = new Swiper('.swiper-container', {
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-	
+	slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
   });
