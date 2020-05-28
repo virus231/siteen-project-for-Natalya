1146446c2714f44c9644ef1def84bea36c7124cc
Vlad <vladprodan6@gmail.com>
2020-05-29 01:30:44 +0300

    Good Luck!

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,40 @@
 $("body").on("mouseenter", "[data-help]", function () {
-    $(".help").removeClass("active");
-    $("#"+$(this).data("help")+"").addClass("active");
+  $(".help").removeClass("active");
+  $("#" + $(this).data("help") + "").addClass("active");
 });
 
 $("body").on("mouseenter", "[data-house]", function () {
-    $(".house").removeClass("active");
-    $("#"+$(this).data("house")+"").addClass("active");
+  $(".house").removeClass("active");
+  $("#" + $(this).data("house") + "").addClass("active");
 });
 
 var swiper = new Swiper('.swiper-container', {
-	slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
+});
+
+$(document).ready(function(){
+  $(".down-block").on("click","a", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+
+      //забираем идентификатор бока с атрибута href
+      var id  = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+          top = $(id).offset().top;
+       
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 3500);
   });
+});
